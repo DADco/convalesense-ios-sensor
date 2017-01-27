8a1ed407ca4d5f8b9d9b40bdf0cf16b5ad9f402e
Spencer MacDonald <spencer.macdonald@gmail.com>
2017-01-27 14:27:06 +0000

    Custom Navigation Bar Font

--- ConvalesenseSensor/AppDelegate.swift
+++ ConvalesenseSensor/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: UIFont(name: "LakkiReddy", size: 22)!]
+    UIBarButtonItem.appearance().setTitleTextAttributes([NSFontAttributeName: UIFont(name: "LakkiReddy", size: 16)!], for: UIControlState.normal)
 
     return true
   }
